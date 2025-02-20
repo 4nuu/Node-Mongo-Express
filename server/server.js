b30dccd143fbc621cadb4a727b393b773378901f
athul <athulrup4@gmail.com>
2025-02-20 11:15:46 +0530

    merged

--- server/server.js
+++ server/server.js
@@ -1,24 +1,24 @@
-require("dotenv").config();
-const mongoose = require ('mongoose');
-const cors = require('cors');
-const express = require('express');
-const app = express();
-const userRoutes = require('./routes/userRoutes');
-const PORT = process.env.PORT || 9000
+// require("dotenv").config();
+// const mongoose = require ('mongoose');
+// const cors = require('cors');
+// const express = require('express');
+// const app = express();
+// const userRoutes = require('./routes/userRoutes');
+// const PORT = process.env.PORT || 9000
 
-app.use(cors());
-app.use(express.json());
-app.use('/user/', userRoutes);
-mongoose.connect(process.env.URL).then( () => console.log("MongoDB Connected Successfully"))
-.catch( (err) => console.log("MongoDB Error",err));
+// app.use(cors());
+// app.use(express.json());
+// app.use('/user/', userRoutes);
+// mongoose.connect(process.env.URL).then( () => console.log("MongoDB Connected Successfully"))
+// .catch( (err) => console.log("MongoDB Error",err));
 
-app.get('/', (req,res) => {
-    res.send("athul")
-})
+// app.get('/', (req,res) => {
+//     res.send("athul")
+// })
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server is running on http://localhost:${PORT}`);
+// });
 
 
 require("dotenv").config();
